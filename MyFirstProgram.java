411e4fdd3418f76e29d0099517e5a37077c8dbf5
Tiana McKee <tiana@consultask.com>
2015-09-12 22:18:35 -0700

    Hello World Program
    first program (hello world) in Java

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+// This is my first Java program
+// the typical hello world prgm
+public class MyFirstProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
